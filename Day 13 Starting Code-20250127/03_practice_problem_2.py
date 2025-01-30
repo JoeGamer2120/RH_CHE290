f9d7702fd2dc8359bebffc1149d11600974ec528
Schlabach <schlabjb@rose-hulman.edu>
2025-01-30 15:33:05 -0500

    Day 13 Practice Problem

--- Day 13 Starting Code-20250127/03_practice_problem_2.py
+++ Day 13 Starting Code-20250127/03_practice_problem_2.py
@@ -28,13 +28,31 @@ def Wilson(L_12, L_21):
     #           - Calculate the activity coefficients the correspond to each
     #             composition and store the results in two separate vectors.
     ###########################################################################
+    x1 = np.linspace(0, 1, 50)
+    x2 = 1 - x1
+    gamma1 = np.zeros(50)
+    gamma2 = np.zeros(50)
+    
+    for i in range(len(x1)):
 
+        
+        gamma1[i] = np.exp(-np.log(L_12*x2[i] + x1[i]) + x2[i]*(L_12/(L_12*x2[i] + x1[i]) - L_21/(L_21*x1[i] + x2[i])))
+        gamma2[i] = np.exp(-np.log(L_21*x1[i] + x2[i]) - x1[i]*(L_12/(L_12*x2[i] + x1[i]) - L_21/(L_21*x1[i] + x2[i])))
     
     ###########################################################################
     # TODO 2: Create figure and axes instances and plot the activity
     #         coefficient of both species vs. x1.
     ###########################################################################
-
+    fig, ax = plt.subplots()
+    
+    ax.plot(x1, gamma1, color = 'blue', ls = '-')
+    ax.plot(x1, gamma2, color = 'orange', ls = '-')
+    
+    ax.set_xlabel('$x^2$')
+    ax.set_ylabel('$\gamma$')
+    ax.set_xlim(0, 1)
+    
+    plt.show()
 
     ###########################################################################
     # TODO 3: This problem is going to illustrate how you can create special
