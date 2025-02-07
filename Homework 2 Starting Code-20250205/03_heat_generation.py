9aa12efaeff43b29e27c62189d0ba554a4d06b58
Josiah <josiahbschlabach@gmail.com>
2025-02-07 09:37:32 -0500

    Homework 2 Problem 3

--- Homework 2 Starting Code-20250205/03_heat_generation.py
+++ Homework 2 Starting Code-20250205/03_heat_generation.py
@@ -5,19 +5,20 @@ statement.
 YOUR NAME: Josiah Schlabach
 """
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
 
 ###############################################################################
-# TODO 1: Define the function below to return the functional values for a 
+# TODO 1: Define the function below to return the functional values for a
 #         system of differential equations.
 ###############################################################################
 
+
 def f(x, y, a):
     return -a
+
 
 def make_plot(x, y, y_e):
     """
@@ -38,17 +39,18 @@ def make_plot(x, y, y_e):
 
     """
     fig, ax = plt.subplots()
-    
-    ax.scatter(x, y, label = "IVP")
-    ax.plot(x, y_e, label = "exact")
-    ax.tick_params(direction = "in")
+
+    ax.scatter(x, y, label="IVP")
+    ax.plot(x, y_e, label="exact")
+    ax.tick_params(direction="in")
     ax.set_xlim(0, 1)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.legend()
 
+
 ###############################################################################
-# TODO 2: Create a vector of equally-spaced x values from 0 to 1 using 21 
+# TODO 2: Create a vector of equally-spaced x values from 0 to 1 using 21
 #         total points.
 ###############################################################################
 x_s = 0
@@ -56,27 +58,30 @@ x_e = 1
 x_span = np.linspace(x_s, x_e, 21)
 
 ###############################################################################
-# TODO 3: For this system, use a = 500 and Tmax = 450. 
+# TODO 3: For this system, use a = 500 and Tmax = 450.
 #         Use solve_ivp to get a solution.
 ###############################################################################
-Tmax=450
+Tmax = 450
 T_0 = Tmax
 T_p = 0
-sol = solve_ivp(f, [x_s, x_e], [T_0, T_p], t_eval=x_span, args = (500, ))
+a = 500
+sol = solve_ivp(f, [x_s, x_e], [T_0, T_p], t_eval=x_span, args=(a,))
 
 
 ###############################################################################
 # TODO 4: Store the result of the integration into its own variable.
 ###############################################################################
-
+x = sol.t
+T = sol.y
 
 ###############################################################################
 # TODO 5: Create a vector of the analytical result.
 ###############################################################################
-
+# T_an = -(a / 2) * x_span**2 + Tmax
+T_an = -(a / 2) * x**2 + Tmax
 
 ###############################################################################
 # TODO 6: Make a plot that compares the numerical and analytical results.
 ###############################################################################
 
-
+make_plot(x_span, T, T_an)
